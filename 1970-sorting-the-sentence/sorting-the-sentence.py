0c44f8ab8d646fda22ffd05dd5a9f5ce7e68ddda
hanannur <hanannuru65@gmail.com>
2025-07-15 20:35:24 +0300

    Time: 49 ms (6.17%) | Memory: 17.7 MB (67.00%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,8 +1,7 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        s_p=s.split()
-        sorted_lst=sorted(s_p,key=lambda x: x[-1])
-        corrected=[i[:-1] for i in sorted_lst]
-        final=' '.join(corrected)
-        return final
+        word=s.split()
+        sorted_word=sorted(word ,key=lambda x : int(x[-1]))
+        current_words=[words[:-1] for words in sorted_word ]
+        return ' '.join(current_words)
 
