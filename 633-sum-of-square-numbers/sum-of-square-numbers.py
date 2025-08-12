9d3fc1ce924f2a38f38879742f4d72b79d6b1641
hanannur <hanannuru65@gmail.com>
2025-08-12 08:31:16 +0300

    Time: 75 ms (37.76%) | Memory: 18 MB (14.79%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        start=0
+        sqrt_value = c ** 0.5
+        end = int(sqrt_value)
+
+        while end>=start:
+            if (start*start)+(end*end)==c:
+                return True
+            elif (start*start)+(end*end)<c:
+                start+=1
+               
+            elif (start*start)+(end*end)>c:
+                end -=1
+        return False
+        
